4209ef84617a77cec14d54b3f6d1d09033ba929b
GrigorevAntonYurievich <37698061+GrigorevAntonYurievich@users.noreply.github.com>
2024-06-09 22:07:38 +0300

    Update SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -20,13 +20,40 @@
  where nametrek like '%my%' or nametrek like '%мой%';
 
 -- Количество исполнителей в каждом жанре.
-select j.hazjz as jzanr, count(distinct t.alid) as artist_count from traks t 
-join jzanr j on t.idjz = j.idjz 
-group by j.hazjz 
+SELECT jzanr.hazjz, count(ispolniteli.nameisp) AS singers_in_genre FROM jzanr
+JOIN artist_genres ON jzanr.idjz = artist_genres.ispid
+JOIN ispolniteli ON artist_genres.ispid = ispolniteli.ispid
+GROUP BY jzanr.hazjz
+ORDER BY count(ispid) DESC;
 
 --Количество треков, вошедших в альбомы 2019–2020 годов.
-SELECT COUNT(*) AS total_tracks
-FROM compilation_tracks
-INNER JOIN artist_albums ON compilation_tracks.sbornik_id = artist_albums.alid 
-WHERE alboms.year BETWEEN 2019 AND 2020;
+SELECT alboms.namealb AS alobom, alboms.godvps, count(traks.nametrek) AS traks_is_albums FROM alboms
+JOIN traks ON traks.alid = alboms.alid
+GROUP BY alboms.namealb, alboms.godvps
+HAVING alboms.godvps BETWEEN '2019' AND '2020';
+
+--Средняя продолжительность треков по каждому альбому.
+SELECT alboms.namealb AS aloboms, avg(traks.dlitel) AS average_songs_duration FROM alboms
+JOIN traks ON traks.alid = alboms.alid
+GROUP BY alboms.namealb
+ORDER BY avg(traks.dlitel);
+
+--Все исполнители, которые не выпустили альбомы в 2020 году.
+ SELECT DISTINCT ispolniteli.nameisp AS ispolniteli FROM ispolniteli
+ WHERE ispolniteli.nameisp NOT IN
+ (SELECT DISTINCT ispolniteli.nameisp AS ispolniteli FROM ispolniteli
+ JOIN artist_albums ON ispolniteli.ispid = artist_albums.ispid
+ JOIN alboms ON artist_albums.alid = alboms.alid
+ WHERE alboms.godvps = 2020)
+ ORDER BY ispolniteli.nameisp;
+
+--Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).
+ SELECT sborniki.sbornik_name AS sbornik1, ispolniteli.nameisp AS ispolniteli FROM sborniki
+ LEFT JOIN compilation_tracks ON sborniki.sbornik_id = compilation_tracks.sbornik_id
+ LEFT JOIN traks ON compilation_tracks.trekid = traks.trekid
+ LEFT JOIN alboms ON traks.alid = alboms.alid
+ LEFT JOIN artist_albums ON alboms.alid = artist_albums.alid
+ LEFT JOIN ispolniteli ON artist_albums.ispid = ispolniteli.ispid
+ WHERE ispolniteli.ispid = 2
+ GROUP BY ispolniteli.nameisp, sborniki.sbornik_name;
 
